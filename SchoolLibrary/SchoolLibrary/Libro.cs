54c4d7595e2b40920f42994b684cbabc6c106f72
Andrei Strugariu <ANDREI.STRUGARIU@local.ittcesena.it>
2025-11-21 08:41:31 +0100

    A0.1.5 : Implementation Of The First Methods

--- SchoolLibrary/SchoolLibrary/Libro.cs
+++ SchoolLibrary/SchoolLibrary/Libro.cs
@@ -8,5 +8,31 @@ namespace SchoolLibrary
 {
     internal class Libro
     {
+        // Attributes
+        public string Titolo { get; set; }
+        public string Autor { get; set; }
+        public string Editore { get; set; }
+        public int AnnoDiPubblicazione{ get; set; }
+        public int NumeroDiPagine { get; set; }
+
+        // Constructor
+        public Libro(string title, string autor, string editor, int yearOfPubblish, int numberOfPages)
+        {
+            Titolo = title;
+            Autor = autor;
+            Editore = editor;
+            AnnoDiPubblicazione = yearOfPubblish;
+            NumeroDiPagine = numberOfPages;
+        }
+        // Method to show book info
+        public void MostraInfo()
+        {
+            Console.WriteLine($"Titolo: {Titolo}");
+            Console.WriteLine($"Autor: {Autor}");
+            Console.WriteLine($"Editore: {Editore}");
+            Console.WriteLine($"Anno Di Pubblicazione: {AnnoDiPubblicazione}");
+            Console.WriteLine($"Numero Di Pagine: {NumeroDiPagine}");
+        }
+
     }
 }
